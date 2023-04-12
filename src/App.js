71361a7c0ba79ad2ae2f17827e3d6b9185242aca
Fell1p <luofelipe@hotmail.com>
2023-04-11 23:48:19 -0300

    Ajustes Card

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ import Formulario from './components/formulario/Formulario'
 
 function App() {
 
-  const [nome, setNome] = useState("Nome Sobrenome")
+  const [nome, setNome] = useState("NOME SOBRENOME")
   const [numeroCard, setNumeroCard] = useState("0000 0000 0000 0000")
   const [mes, setMes] = useState("00")
   const [ano, setAno] = useState("00")
   const [cvc, setCvc] = useState("000")
-
+ 
   return (
     <div className="App">
      <div className='app-card-style'>
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -14,10 +14,10 @@ const Card = ({ nome, numeroCard, mes, ano, cvc }) => {
         <div className='bandeiraTransparente-style'></div>
       </div>
       <div className='style-numero-card' >
-        {numeroCard ? (<p>{cartaoInfos(numeroCard)}</p>) : (<p>0000 0000 0000 0000</p>)}
+        {numeroCard ? (<p>{cartaoInfos(numeroCard)}</p>) : (<p>0000  0000  0000  0000</p>)}
       </div>
       <div className='styles-nome-card' >
-        {nome ? (<p>{cartaoInfos(nome)}</p>) : (<p>Nome Sobrenome</p>)}
+        {nome ? (<p>{cartaoInfos(nome)}</p>) : (<p>NOME SOBRENOME</p>)}
       </div>
       <div className='card-validade-style' >
         {mes ? (<p>{cartaoInfos(mes)}/</p>) : (<p>00</p>)}
