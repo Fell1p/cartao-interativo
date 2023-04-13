722330f8a4b0c2dd212233dd3a0aa496253a21c6
Fell1p <luofelipe@hotmail.com>
2023-04-12 23:14:08 -0300

    Estilização do verso do cartão

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -9,22 +9,32 @@ const Card = ({ nome, numeroCard, mes, ano, cvc }) => {
 
   return (
     <div className='card-style-container' >
-      <div className='bandeira-style-continer'>
-        <div className='bandeiraSolida-style'></div>
-        <div className='bandeiraTransparente-style'></div>
+      <div className='card-container-front' >
+        <div className='bandeira-style-continer'>
+          <div className='bandeiraSolida-style'></div>
+          <div className='bandeiraTransparente-style'></div>
+        </div>
+        <div className='style-numero-card' >
+          {numeroCard ? (<p>{cartaoInfos(numeroCard)}</p>) : (<p>0000  0000  0000  0000</p>)}
+        </div>
+        <div className='container-name-validade'>
+          <div className='styles-nome-card' >
+            {nome ? (<p>{cartaoInfos(nome)}</p>) : (<p>NOME SOBRENOME</p>)}
+          </div>
+          <div className='card-validade-style' >
+            {mes ? (<p>{cartaoInfos(mes)}/</p>) : (<p>00</p>)}
+            {ano ? (<p>{cartaoInfos(ano)}</p>) : (<p>00</p>)}
+          </div>
+        </div>
       </div>
-      <div className='style-numero-card' >
-        {numeroCard ? (<p>{cartaoInfos(numeroCard)}</p>) : (<p>0000  0000  0000  0000</p>)}
-      </div>
-      <div className='styles-nome-card' >
-        {nome ? (<p>{cartaoInfos(nome)}</p>) : (<p>NOME SOBRENOME</p>)}
-      </div>
-      <div className='card-validade-style' >
-        {mes ? (<p>{cartaoInfos(mes)}/</p>) : (<p>00</p>)}
-        {ano ? (<p>{cartaoInfos(ano)}</p>) : (<p>00</p>)}
-      </div>
-      <div className='style-cvc-card' >
-        {cvc ? (<p>{cartaoInfos(cvc)}</p>) : (<p>000</p>)}
+      <div className='card-container-back'>
+        <div className='tarja-style'></div>
+        <div className='style-cvc-card' >
+          {cvc ? (<p className='cvc'>{cartaoInfos(cvc)}</p>) : (<p className='cvc'>000</p>)}
+        </div>
+        <span>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda, asperiores?
+        </span>
       </div>
     </div>
   )
